eb3d2a2a690919f2378e8fd256ec4ffdb87c8cf1
bjimison <brady.t.jimison@gmail.com>
2019-11-07 12:35:21 -0800

    laying groundwork for pulling response details out of response object

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // const myImage = document.querySelector('.my-image');
+
 const fetch = require("node-fetch");
 const authToken = "bearer 7zF8BOnG4G7RzWhxfQxo2YjP7B-O0myhv7uUtPX8DL9gNqXvZvY.YfjdXqHQODdWf11OQQl6WPmRbeerKXG6Jwh0SMpOUsuVFMliRcw2GeifUQAwfGCdOu.Qp1F9-QFC";
-const url = 'https://api.surveymonkey.com/v3/surveys/272379092/details'
+const getSurveyIdURL = 'https://api.surveymonkey.com/v3/surveys'
+const getSurveyDetailsURL = 'https://api.surveymonkey.com/v3/surveys/272379092/details'
+const getSurveyResponsesURL = 'https://api.surveymonkey.com/v3/surveys/272379092/responses/bulk'
 
-function getSurvey() {
+// call '/surveys' endpoint with only Auth Token to receive the ID's of the surveys associated with an account
+function getSurveyId() {
     let response;
-    response = fetch(url, {
+    response = fetch(getSurveyIdURL, {
         method: 'GET',
         headers: { 'Authorization': authToken }
     })
@@ -17,7 +21,43 @@ function getSurvey() {
         .catch(error => console.log(error));
 }
 
-getSurvey();
+// call '/surveys/{id}/details' with Auth Token and the survey Id in the URL to receive survey setup details
+function getSurveyDetails() {
+    let response;
+    response = fetch(getSurveyDetailsURL, {
+        method: 'GET',
+        headers: { 'Authorization': authToken }
+    })
+        .then(res => res.json());
+    console.log(response);
+    response.then(function (result) {
+        console.log(result) // survey response details
+        // info on questions, answer options, survey types, etc are embedded in an array in the 'Pages' response field
+    })
+        .catch(error => console.log(error));
+}
+
+// call '/surveys/{id}/responses/bulk' with Auth Token and survey ID to receive survey response details
+function getSurveyResponse() {
+    let response;
+    response = fetch(getSurveyResponsesURL, {
+        method: 'GET',
+        headers: { 'Authorization': authToken }
+    })
+        .then(res => res.json());
+    console.log(response);
+    response.then(function (result) {
+        console.log(result) // survey response details
+        // questions and answer details, text, etc. are embedded in an array in the 'Pages' response field
+        // for questions with button selections pages[0].questions[0].answers[0].choice_id
+        // for questions requiring text response: pages[0].questions[0].answers[0].text
+    })
+        .catch(error => console.log(error));
+}
+
+getSurveyId();
+getSurveyDetails();
+getSurveyResponse();
 
     // potentially useful code
     // .then(res => {
